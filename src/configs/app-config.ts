e178cf79cf9664b60bf19a69fd51d29d39fdaaa8
Sandarsh Sridhar <sandarsh.sridhar94@gmail.com>
2022-11-13 11:50:37 -0600

    doc: readme updated with usage instructions

--- src/configs/app-config.ts
+++ src/configs/app-config.ts
@@ -4,5 +4,5 @@ dotenv.config();
 
 export const appConfig = {
   port: process.env.APP_PORT || '1251',
-  redirectUri: process.env.REDIRECT_URI || 'http://localhost:1251/api/callback'
+  redirectUri: process.env.REDIRECT_URI || 'http://localhost:1251/callback'
 };
