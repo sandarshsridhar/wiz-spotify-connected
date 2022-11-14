ff0ddd21a8070a81dac66fb2bfcb6ea2c802102e
Sandarsh Sridhar <sandarsh.sridhar94@gmail.com>
2022-11-14 15:27:46 -0600

    fix: change the port to 8888 for ease of use

--- src/configs/app-config.ts
+++ src/configs/app-config.ts
@@ -3,6 +3,6 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 export const appConfig = {
-  port: process.env.APP_PORT || '1251',
-  redirectUri: process.env.REDIRECT_URI || 'http://localhost:1251/callback'
+  port: process.env.APP_PORT || '8888',
+  redirectUri: process.env.REDIRECT_URI || 'http://localhost:8888/callback'
 };
